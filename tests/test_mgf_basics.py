8a4c6a4b67d6817607cd8f225fd73d8442380839
Jesse Windle <jesse@bayesfactor.net>
2023-06-20 13:29:41 -0500

    Updated test_mgf_basics

--- tests/test_mgf_basics.py
+++ tests/test_mgf_basics.py
@@ -13,3 +13,14 @@ def test_loss_components(model_and_data):
     assert np.all(np.isclose(lc1, lc2))
 
 
+def test_opt_I_runs(model_and_data):
+    mod, data = model_and_data
+    theta_init = data['theta0']
+    mod.opt_I(theta_init)
+
+
+def test_opt_runs(model_and_data):
+    mod, data = model_and_data
+    theta_init = data['theta0']
+    W = np.identity(mod.num_moments)
+    mod.opt(theta_init, W)
