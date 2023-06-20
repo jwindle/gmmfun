2639b1b4ab2fdb6765fa2a8b8ad65d693857489c
Jesse Windle <jesse@bayesfactor.net>
2023-06-20 13:29:59 -0500

    Updated test mgf by comparisons

--- tests/test_mgf_by_comparison.py
+++ tests/test_mgf_by_comparison.py
@@ -4,12 +4,11 @@ import pytest
 import numpy as np
 
 
-def test_similar_output(all_models_and_data):
-    models, data = all_models_and_data
-    for name, mod in models.items():
-        mod.opt_I(data['theta0'])
-        mod.update_both(10)
-    for name, mod in models.items():
-        print(name)
-        # These shouldn't produce identical results, just close, unless we have lots of data
-        assert np.all(np.isclose(mod.theta, models['cgf_4'].theta, atol=1e-2))
+def test_similar_output(model_pair_and_data):
+    mod0, mod1, data = model_pair_and_data
+    mod0.opt_I(data['theta0'])
+    mod0.update_both(10)
+    mod1.opt_I(data['theta0'])
+    mod1.update_both(10)
+    # These shouldn't produce identical results, just close, unless we have lots of data
+    assert np.all(np.isclose(mod1.theta, mod0.theta, atol=1e-1))
